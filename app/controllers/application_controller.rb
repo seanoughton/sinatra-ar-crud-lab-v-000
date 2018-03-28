02f1b91061a9da462fda272e17fc5573cc43e1b1
Sean Oughton <seanoughton@gmail.com>
2018-03-28 22:22:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
     @post.name = params[:name]
     @post.content = params[:content]
     @post.save
-    redirect to '/posts/#{@post.id}'
+    redirect to "/posts/#{@post.id}"
   end
 
 end
